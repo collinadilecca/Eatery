e10d5b0e3c216a09be311feec615f3b69e38c1de
Nikola Djikic <nikoladjikic22@gmail.com>
2020-09-08 19:33:59 +0200

    Add drinks component, styles and basic dashboard component

--- Eatery/src/app/components/drinks/drinks.component.ts
+++ Eatery/src/app/components/drinks/drinks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Drinks } from 'src/assets/drinks';
 
 @Component({
   selector: 'app-drinks',
@@ -11,5 +12,36 @@ export class DrinksComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  water: Drinks = {
+    name: 'Water',
+    price: 2,
+    url: 'https://icon2.cleanpng.com/20171220/kxq/water-bottle-png-image-5a3ac0c436d735.8291154815137998762246.jpg'
+  }
+
+  cola: Drinks = {
+    name: 'Cola',
+    price: 4,
+    url: 'https://pngimg.com/uploads/cocacola/cocacola_PNG3.png'
+  }
+
+  orangeJuice: Drinks = {
+    name: 'Orange juice',
+    price: 5,
+    url: 'https://icon2.cleanpng.com/20180130/kaw/kisspng-orange-juice-smoothie-jal-jeera-oranges-and-orange-juice-splash-5a7025e81d4cf3.78988170151729917612.jpg'
+  }
+
+  appleJuice: Drinks = {
+    name: 'Apple juice',
+    price: 5,
+    url: 'https://img.pngio.com/apple-juice-png-images-free-png-library-apple-juice-png-418_385.png'
+  }
+
+  beer: Drinks = {
+    name: 'Beer',
+    price: 9,
+    url: 'https://lh3.googleusercontent.com/proxy/vxOk8FGJJg1YpClWzs_TP1UwbDrYHk3_Wum3s70HoitNzlD7snGottcKSENEzlWr36q-K16zZq7Z0RYiowVCK0NgebRzVSVsNLBpPg_97ajdNLleDoGp'
+  }
+
 
 }
--- Eatery/src/assets/drinks.ts
+++ Eatery/src/assets/drinks.ts
@@ -1,4 +1,4 @@
-export interface Food {
+export interface Drinks {
     name: string,
     url: string,
     price: number
